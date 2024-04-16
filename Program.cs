a041b9224601181f39ab4f306bd893c88da12f93
chanbito <gabrielmarafiga@hotmail.com>
2024-04-16 20:02:44 -0300

    aplicada alterações necessarias

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "12", modelo:"mod", iMEI:"111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WPP");
+
+
+Smartphone iphone = new Iphone(numero: "12", modelo: "mod", iMEI: "111111111", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("telegrão");
